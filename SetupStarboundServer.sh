2246ec95b8eb914e162be17b44e7e742c97ff796
k12 <k12@NovaNetNode7.ikard.online>
2016-01-25 23:27:53 -0700

    Copied to the wrong directory.  Now installs it to ~/Steam/steamapps/common/Starbound

--- SetupStarboundServer.sh
+++ SetupStarboundServer.sh
@@ -6,7 +6,7 @@ printf "%s" "Enter your steam password: "; read -s STEAMPASSWORD
 
 #----------- Install dependencies and directories ------------------------
 sudo apt-get -y install lib32gcc1 libpng12-0
-mkdir ~/steamcmd 2>/dev/null
+mkdir -p ~/Steam/steamapps/common/Starbound/ 2>/dev/null
 cd ~/steamcmd
 
 #-----------If steamcmd.sh doesn't exist, download it --------------------
@@ -21,7 +21,7 @@ sudo bash -c "cat << EOF > /usr/local/bin/starboundserver
 #!/bin/bash
 case \"\\\$1\" in
 start)
-cd /home/k12/Steam/linux64
+cd ~/Steam/steamapps/common/Starbound/linux64
 ./starbound_server &
 ;;
 stop)
@@ -57,6 +57,6 @@ sudo systemctl daemon-reload
 sudo systemctl enable starboundserver.service
 
 #----------- Run SteamCMD to update or install ---------------------------
-./steamcmd.sh +login $STEAMUSERNAME $STEAMPASSWORD +force_install_dir /home/k12/Steam +app_update 211820 +quit
+./steamcmd.sh +login $STEAMUSERNAME $STEAMPASSWORD +force_install_dir ~/Steam/steamapps/common/Starbound +app_update 211820 +quit
 
 exit 0
